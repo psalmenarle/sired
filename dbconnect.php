7dfc8605decb78bd259bac48436ec01a553da7bc
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-03-31 17:14:53 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
 $conn = mysqli_connect("localhost","root","") or die (mysqli_error($conn));
-$db = mysqli_select_db($conn,"db_bank");
+$db = mysqli_select_db($conn,"heroku_5e9ae621cca5225");
 ?>
