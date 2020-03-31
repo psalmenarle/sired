3473a946c9a4b60529d7a6ccfcf41dccea4ef857
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-03-31 17:21:42 +0800

    Update weekly.php

--- weekly.php
+++ weekly.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_connect("localhost","root","") or die (mysqli_error($conn));
-$db = mysqli_select_db($conn,"db_bank");
+$db = mysqli_select_db($conn,"heroku_5e9ae621cca5225");
 
 
 $sql = "SELECT * FROM tblallowance";
