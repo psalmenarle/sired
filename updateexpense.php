9833d2d694624ecd6354e76318bd6fd017ae4015
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-03-31 17:48:32 +0800

    Update updateexpense.php

--- updateexpense.php
+++ updateexpense.php
@@ -1,7 +1,7 @@
 <?php
   $exp_id = $_GET ['id'];
   
-$conn = mysqli_connect("localhost","root","") or die (mysqli_error($conn));
+$conn = mysqli_connect("localhost","b2d9ee006dc0a9","38ea438c") or die (mysqli_error($conn));
 $db = mysqli_select_db($conn,"heroku_5e9ae621cca5225");
 
 $sql = "SELECT * FROM tblexpense WHERE exp_id = $exp_id";
