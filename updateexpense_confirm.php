62984cdfd39464d7c8b241005d037412c83e0f05
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-03-31 17:48:59 +0800

    Update updateexpense_confirm.php

--- updateexpense_confirm.php
+++ updateexpense_confirm.php
@@ -5,7 +5,7 @@ $expname = $_POST['expname'];
 $budget = $_POST['budget'];
 
 
-$conn = mysqli_connect("localhost","root","") or die (mysqli_error($conn));
+$conn = mysqli_connect("localhost","b2d9ee006dc0a9","38ea438c") or die (mysqli_error($conn));
 $db = mysqli_select_db($conn,"heroku_5e9ae621cca5225");
 
 $sql = "UPDATE tblexpense SET exp_name = '$expname', budget = $budget WHERE exp_id = $exp_id " ; 
