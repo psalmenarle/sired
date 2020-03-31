cd35de3ec4cb755d60540c122466d808709fed33
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-03-31 17:21:57 +0800

    Update weeklybud.php

--- weeklybud.php
+++ weeklybud.php
@@ -3,7 +3,7 @@
 $allow_id = $_GET['allow_id'];
 
 $conn = mysqli_connect("localhost","root","") or die (mysqli_error($conn));
-$db = mysqli_select_db($conn,"db_bank");
+$db = mysqli_select_db($conn,"heroku_5e9ae621cca5225");
 
 
 
